9a323b07fc9513d053831925842ba946a173c381
mchrysl <mchrys@wgu.edu>
2022-12-18 15:13:48 -0500

    Added short input file to project for testing Also added println statements to see what's going on in the program

--- src/RadioIssues.java
+++ src/RadioIssues.java
@@ -10,50 +10,59 @@ public class RadioIssues {
         String inputBuffer = "";
         try {
             //Read in the string of characters
-            Scanner inputReader = new Scanner(new File("inputDay6Short.txt"));
+            Scanner inputReader = new Scanner(new File("inputDay6.txt"));
             while(inputReader.hasNextLine()){
                 inputBuffer = inputBuffer + inputReader.nextLine();
             }
 
+            //Note: Part one requires 4 consecutive unique characters, Part two requires 14
             String possibleMarker = "";
             String possibleMarkerComp = "";
             int markerStartIndex = 0;
-            int markerEndIndex = 3;
+            int markerEndIndex = 13;
             int markerDuplicateIndex = -1;
             char compChar = 9;
+            boolean duplicateFound = false;
 
             while(markerEndIndex <= inputBuffer.length()){
-                System.out.println("Top of loop");
-                //pull the 4 characters that are the possible start marker
-                for(int i = 0; i < 4; i++){
-                    possibleMarker = possibleMarker + inputBuffer.charAt(markerStartIndex+i);
+                System.out.println("Top of loop: markerStartIndex=" + markerStartIndex + " markerEndIndex=" + markerEndIndex);
+                //pull the 4 characters that are the possible marker of the start
+                if(markerEndIndex == inputBuffer.length()){
+                    possibleMarker = inputBuffer.substring(markerStartIndex);
+                }else {
+                    possibleMarker = inputBuffer.substring(markerStartIndex, markerEndIndex + 1);
                 }
                 System.out.println("possibleMarker: " + possibleMarker);
                 //see if there are duplicates in possibleMarker
-                for(int mc = 0; mc < 4; mc++) {
+                for(int mc = 0; mc < 14; mc++) {
                     compChar = possibleMarker.charAt(mc);
                     System.out.println("compChar: " + compChar);
                     //compare to possibleMarker string minus the compChar (replace with 0)
                     if(mc == 0){
                         possibleMarkerComp = "0" + possibleMarker.substring(mc+1);
                     }else {
-                        possibleMarkerComp = possibleMarker.substring(0, mc + 1) + "0" + possibleMarker.substring(mc + 1);
+                        possibleMarkerComp = possibleMarker.substring(0, mc) + "0" + possibleMarker.substring(mc + 1);
                     }
                     System.out.println("possibleMarkerComp string: " + possibleMarkerComp);
-                    /*
-                    markerDuplicateIndex = possibleMarker.indexOf(compChar);    //note: -1 means does not exist
-                    if (markerDuplicateIndex > -1) {  //duplicate is found
-                        //increment markerStartIndex & markerEndIndex by 1, reset the other variables
-                        markerStartIndex++;
-                        markerEndIndex++;
-                        possibleMarker = "";
-                        possibleMarkerComp = "";
-                        compChar = 9;
-                    } else {      //duplicate not found ==> this is the marker
-                        System.out.println("First marker after character: " + markerEndIndex);
-                    }
 
-                     */
+                    markerDuplicateIndex = possibleMarkerComp.indexOf(compChar);    //note: -1 means dup does not exist
+                    System.out.println("markerDuplicateIndex=" + markerDuplicateIndex);
+                    if (markerDuplicateIndex > -1) {  //duplicate found
+                        duplicateFound = true;
+                        break;
+                    }
+                }//end for
+
+                if(duplicateFound == true){
+                    //increment start/end index and reset duplicateFound to false
+                    markerStartIndex++;
+                    markerEndIndex++;
+                    duplicateFound = false;
+                }else{
+                    System.out.println("**************************************************");
+                    System.out.println("First marker after character: " + (markerEndIndex+1));
+                    System.out.println("**************************************************");
+                    System.exit(10);
                 }
             }
 
